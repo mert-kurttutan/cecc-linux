f3225e69e74c5fe1f54b006a4a2433830e6e5439
mert-kurttutan <kurttutan.mert@gmail.com>
2026-06-22 22:58:06 +0300

    update gitignore

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Dev shell for Casper WMI kernel driver work";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          kernelPackages = pkgs.linuxPackages;
+          kernel = kernelPackages.kernel;
+          kdir = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nushell
+              gnumake
+              gcc
+              binutils
+              elfutils
+              kmod
+              pkg-config
+            ] ++ kernel.moduleBuildDependencies ++ [
+              kernel.dev
+            ];
+
+            shellHook = ''
+              export DRIVER_DIR="$PWD/casper-wmi"
+              export KDIR="${kdir}"
+              export MODULE_NAME="casper_wmi"
+
+              echo "driver dir: $DRIVER_DIR"
+              echo "kernel build dir: $KDIR"
+              echo "reload loop: nu ./scripts/reload.nu"
+              echo "override kernel dir if needed: KDIR=/path/to/build nu ./scripts/reload.nu"
+            '';
+          };
+        });
+    };
+}
